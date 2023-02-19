8bc13d2e401826756e96699766746d5f18437276
08mterrell <ammn2000@gmail.com>
2023-02-19 02:21:36 -0500

    test

--- src/Maze.java
+++ src/Maze.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Maze {
 
     protected char[][] m;
@@ -9,55 +11,43 @@ public class Maze {
     }
 
     public void escape(int row, int col) {
+        boolean istrue = true;
+        boolean isTrue = true;
+        while (isTrue) {
+            Scanner in = new Scanner(System.in);
+            int cont = in.nextInt();
 
 
-        boolean rightEmpty = true;
-        boolean leftEmpty = true;
-        boolean upEmpty = true;
-        boolean downEmpty = true;
 
-        boolean end = false;
-        if(!end) {
-            if (row <= 8) {
-                if (Character.compare(m[row][col], '*') == 0) {
-                    downEmpty = false;
-                }
-            }
-            if (row >= 0) {
-                if (Character.compare(m[row][col], '*') == 0) {
-                    upEmpty = false;
-                }
-            }
-            if (col >= 0) {
-                if (Character.compare(m[row][col], '*') == 0) {
-                    leftEmpty = false;
-                }
-            }
-            if (col <= 8) {
-                if (Character.compare(m[row][col], '*') == 0) {
-                    rightEmpty = false;
-                }
-            }
-
-            if (downEmpty) {
+            if (Character.compare(m[row][col], '*') != 0) { // check if current position is empty space
                 m[row][col] = '+';
-                row += 1;
+                if (Character.compare(m[row + 1][col], '*') != 0) { //check down for empty space
+                    row += 1;
+                    m[row][col] = '+';
+                } else if (Character.compare(m[row + 1][col], '+') != 0) { //deletes "+" if down is full
+                    row -= 1;
+                    m[row][col] = ' ';
+                }
+                if (Character.compare(m[row][col + 1], '*') != 0) { // check to right for empty space
+                    col += 1;
+                    m[row][col] = '+';
+                } else if (Character.compare(m[row][col + 1], '+') != 0) { //deletes "+" if right is full
+                    col -= 1;
+                    m[row][col] = ' ';
+                }
+                if (Character.compare(m[row][col - 1], '*') != 0) { // checks to the left for empty space
+                    col -= 1;
+                    m[row][col] = '+';
+                }
+                if (Character.compare(m[row - 1][col], '*') != 0 && row >= 0) { // checks up for empty space
+                    row -= 1;
+                    m[row][col] = '+';
+                }
+                if (cont == 2) {
+                    isTrue = false;
+                }
+
             }
-            if (rightEmpty) {
-                m[row][col] = '+';
-                col += 1;
-            }
-            if (row == 8) {
-                end = true;
-            }
-            else {
-                return;
-            }
-            escape(row, col);
         }
-
-
-        System.out.print(downEmpty);
-
     }
 }
--- src/MazeDemo.java
+++ src/MazeDemo.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
+
 public class MazeDemo
 {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         char[][] m = {
                 {'*', ' ', '*', '*', '*', '*', '*', '*', '*'},
                 {'*', ' ', ' ', ' ', ' ', ' ', '*', ' ', '*'},
@@ -13,13 +14,15 @@ public class MazeDemo
                 {'*', ' ', ' ', ' ', ' ', ' ', '*', ' ', '*'},
                 {'*', '*', '*', '*', '*', '*', '*', ' ', '*'}};
         Maze maze = new Maze(m);
-        maze.escape(0, 1);
-        System.out.println("The path from the starting point to the exit is drawn with \"+\" below:");
-        for (int i = 0; i < maze.height; i++) {
-            for (int j = 0; j < maze.width; j++) {
-                System.out.print(m[i][j]);
+
+
+            maze.escape(0, 1);
+            System.out.println("The path from the starting point to the exit is drawn with \"+\" below:");
+            for (int i = 0; i < maze.height; i++) {
+                for (int j = 0; j < maze.width; j++) {
+                    System.out.print(m[i][j]);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-}
